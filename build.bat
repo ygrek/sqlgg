5d67b4e0f3ac84e900b0d5f30cb81260822b3f84
ygrek <ygrek@autistici.org>
2009-04-23 22:11:22 +0300

    move some flags to _tags

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 del sql2cpp.exe
 git rev-parse HEAD | ocaml make_version.ml > version.ml
-ocamlbuild -classic-display -no-links -cflags -dtypes,-w,Az sql2cpp.byte
+ocamlbuild -classic-display -no-links sql2cpp.byte
 copy _build\sql2cpp.byte sql2cpp.exe
