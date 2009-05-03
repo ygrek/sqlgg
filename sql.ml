76c76e44528c06bba50fe03c79e33e6fb7176e74
ygrek <ygrek@autistici.org>
2009-05-03 22:51:27 +0300

    remove old code

--- sql.ml
+++ sql.ml
@@ -21,21 +21,3 @@ struct
        deriving (Show)
 end
 
-module Col =
-struct
-  type t = {name:string; cpp_name:string; sqltype:Type.t; constraints:Constraint.t list}
-       deriving (Show)
-  let make name sqltype constraints = 
-    {name=name; cpp_name=name; sqltype=sqltype; constraints=constraints}
-  let type_to_string c = Type.to_string c.sqltype
-  let type_to_cpp_string c = Type.to_cpp_string c.sqltype
-  let is_primary_key c = List.mem Constraint.PrimaryKey c.constraints
-end
-
-module Table =
-struct
-  type t = {name:string; cpp_name:string; cols:Col.t list}
-       deriving (Show)
-
-  let make name cols = {name=name;cpp_name=name;cols=cols}
-end
