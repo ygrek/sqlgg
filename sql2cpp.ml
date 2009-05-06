134035a562c3cc37327831cc3cffaeed3fccb358
ygrek <ygrek@autistici.org>
2009-05-06 20:23:50 +0300

    + -version

--- sql2cpp.ml
+++ sql2cpp.ml
@@ -7,10 +7,12 @@ open Operators
 let work filename = 
   Main.with_file filename Main.parse_sql
 
-let show_help () =
-  Error.log "SQL to C++ Code Generator Version %s (%s)" 
+let version = Printf.sprintf "%s (%s)" 
     Version.version 
-    (Version.revision >> String.explode >> List.take 8 >> String.implode);
+    (Version.revision >> String.explode >> List.take 8 >> String.implode)
+
+let show_help () =
+  Error.log "SQL to C++ Code Generator Version %s" version;
   Error.log "";
   Error.log " Usage: %s file_with_statements.sql" (Filename.basename Sys.executable_name);
   Error.log "";
@@ -19,8 +21,9 @@ let show_help () =
 let main () =
   match Array.to_list Sys.argv with
   | _::"-test"::_ -> Test.run ()
-  | _::["-"] -> Main.parse_sql (Std.input_all stdin)
-  | _::[file] -> work file
+  | _::"-version"::_ -> print_endline version
+  | _::"-"::_ -> Main.parse_sql (Std.input_all stdin)
+  | _::file::_ -> work file
   | _ -> show_help ()
 
 let _ = Printexc.print main ()
