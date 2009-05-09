e3ac4d5352d820fd2f33d2cf336a634255df9b97
ygrek <ygrek@autistici.org>
2009-05-09 08:25:23 +0300

    comments back

--- real.sql
+++ real.sql
@@ -7,12 +7,12 @@
    (
     select code
     from 
-      (select code, descript
+      (select code, descript     // gets unique code-and-descript combos
        from codeTable
        where locat not in (@lim1, @lim2)
        group by code, descript
       )
     group by code
-    having count(*) > @x
+    having count(*) > @x     // (error? should be "="?)
    )
  order by code, locat;
