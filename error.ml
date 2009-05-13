1e25211f41e5530e755be3ff9f616d7836f730c6
ygrek <ygrek@autistici.org>
2009-05-13 18:35:25 +0300

    minor

--- error.ml
+++ error.ml
@@ -1,11 +1,12 @@
-(*
-  $Id$
-*)
+(** Error output *)
 
 open Printf
 
 let logs = prerr_endline
 let log format = ksprintf logs format
-let report format = ksprintf (fun str -> logs ("Project : warning _____: " ^ str)) format
+(* let report format = ksprintf (fun str -> logs ("Project : warning _____: " ^ str)) format *)
+let report = log
 
-(* path(lineno) : warning _____: string*)
+(* VS error format: 
+  path(lineno) : warning _____: string
+*)
