f3f6b009a6e11a4895f40ac2936ba3c0f9f794cd
ygrek <ygrek@autistici.org>
2009-05-23 13:44:07 +0300

    need to change generator to account for mysql

--- impl/mysql_traits.hpp
+++ impl/mysql_traits.hpp
@@ -16,34 +16,77 @@ struct mysql_traits
   typedef std::basic_string<TCHAR> Text;
   typedef Text Any;
 
-  typedef MYSQL_STMT* statement;
+  template<size_t count>
+  struct rowT
+  {
+    rowT(MYSQL_STMT* stmt) : stmt(stmt)
+    {
+      memset(bind,0,sizeof(bind));
+      memset(length,0,sizeof(length));
+      memset(error,0,sizeof(error));
+      memset(is_null,0,sizeof(is_null));
+    }
+
+    MYSQL_STMT* stmt;
+    MYSQL_BIND bind[count];
+    unsigned long length[count];
+    my_bool error[count];
+    my_bool is_null[count];
+  }
+
+  typedef row& row;
   typedef MYSQL* connection;
 
-  /*
-     template<class T>
-     static void get_column_Int(statement stmt, int index, T& val)
-     {
-     val = sqlite3_column_int(stmt, index);
-     }
+  template<class T>
+  static void get_column(statement stmt, int index, T& data)
+  {
+    // nothing
+  }
 
-     template<class T>
-     static void get_column_Text(statement stmt, int index, T& val)
-     {
-#if defined(_UNICODE) || defined(UNICODE)
-val = (const TCHAR*)sqlite3_column_text16(stmt, index);
-#else   
-val = (const TCHAR*)sqlite3_column_text(stmt, index);
-#endif   
+  template<>
+  static void get_column(statement stmt, int index, Text& data)
+  {
+    unsigned long const& length = stmt.length[index];
+    MYSQL_BIND& bind = stmt.bind[index];
+
+    if (length > 0)
+    {
+      data.resize(length);
+      bind.buffer = data.c_str();
+  bind.buffer_length = length;
+  mysql_stmt_fetch_column(stmt, stmt.bind, index, 0);
 }
-*/
+
+    data.resize(stmt.length[index])
+  }
+
+  static void bind_column(statement stmt, int index, Int& data)
+  {
+    MYSQL_BIND& bind = stmt.bind[index];
+
+    bind.buffer_type = MYSQL_TYPE_LONG;
+    bind.buffer = (void*)&data;
+    bind.is_null = &stmt.is_null[index];
+    bind.length = &stmt.length[index];
+    bind.error = &stmt.error[index];
+  }
+
+  static void bind_column(statement stmt, int index, Text& data)
+  {
+    // FIXME
+    data.resize(1024);
+
+    MYSQL_BIND& bind = stmt[index];
+
+    bind.buffer_type = MYSQL_TYPE_STRING;
+    bind.buffer = data.c_str();
+    bind.buffer_length = 1024;
+    bind.is_null = &stmt.is_null[index];
+    bind.length = &stmt.length[index];
+    bind.error = &stmt.error[index];
+  }
 
   /*
-     static void set_param_null(statement stmt, int index)
-     {
-     int nResult = sqlite3_bind_null(stmt, index + 1);
-     ATLASSERT(SQLITE_OK == nResult);
-     }
-
      static void set_param_Text(statement stmt, const Text& val, int index)
      {
 #if defined(_UNICODE) || defined(UNICODE)
@@ -101,13 +144,16 @@ static bool do_select(connection db, Container& result, const TCHAR* sql, Binder
     return false;
   }
 
-//  result.clear();
+  row<Binder::count> data;
+  Container::value_type val;
+  binder.bind(data,val);
 
-//  while(SQLITE_ROW == sqlite3_step(stmt)) //Iterate all objects
-//  {
-//    result.push_back(typename Container::value_type());
-//    binder.of_stmt(stmt,result.back());
-//  }
+  result.clear();
+  while (0 == mysql_stmt_fetch(stmt))
+  {
+    binder.get(data,val);
+    result.push_back(val);
+  }
 
   //Destroy the command
   if (mysql_stmt_close(stmt))
