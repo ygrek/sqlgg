fa7af95b2786124c34262f6a2967ee7055c25146
ygrek <ygrek@autistici.org>
2009-05-25 15:28:53 +0300

    gen_java compiles

--- gen_java.ml
+++ gen_java.ml
@@ -14,69 +14,63 @@ module G = Gen_cxx
 let comment = G.comment
 let empty_line = G.empty_line
 
-let start_class name =
-   output "public class %s" name;
+let start_ cls =
+   let f1 name =
+   output "public %s %s" cls name;
    G.open_curly ()
-
-let end_class name =
+   in
+   let f2 name =
    G.close_curly " // class %s" name;
    empty_line ()
+   in
+   f1,f2
 
-let set_column attr index =
-  output "Traits::set_column_%s(stmt, %u, obj.%s);" 
-    (Type.to_string attr.RA.domain)
-    index
-    (name_of attr index)
+let (start_class,end_class) = start_ "class"
+let (start_intf,end_intf) = start_ "interface"
+
+let as_java_type = function
+  | Type.Int -> "int"
+  | Type.Text -> "String"
+  | Type.Float -> "float"
+  | Type.Blob -> "Blob"
+  | Type.Bool -> "boolean"
+  | Type.Datetime -> "Timestamp"
 
 let get_column attr index =
-  output "Traits::get_column_%s(stmt, %u, obj.%s);" 
-    (Type.to_string attr.RA.domain)
-    index
-    (name_of attr index)
+  sprintf "res.get%s(%u)"
+    (attr.RA.domain >> as_java_type >> String.capitalize)
+    (index + 1)
 
-let param_type_to_string t = Option.map_default Type.to_string "Any" t
-let as_cxx_type str = "typename Traits::" ^ str
+let param_type_to_string t = t >> Option.default Type.Text >> as_java_type
 
 let set_param index param =
   let (id,t) = param in
-  output "Traits::set_param_%s(stmt, %s, %u);" 
-    (param_type_to_string t)
-    (param_name_to_string id index)
+  output "pstmt.set%s(%u, %s);"
+    (t >> param_type_to_string >> String.capitalize)
     index
+    (param_name_to_string id index)
 
-(*
-public interface ServerCallback
-{
-public void callback(String arg);
-}
-*)
+let schema_to_values = List.mapi (fun i attr -> name_of attr i, attr.RA.domain >> as_java_type)
 
 let output_schema_binder index schema =
   let name = default_name "output" index in
-  output "template <class T>";
-  start_class name;
+  start_intf name;
 
-  output "public static void of_stmt(statement stmt, T& obj)";
-  G.open_curly ();
-  List.iteri (fun index attr -> get_column attr index) schema;
-  G.close_curly "";
+  output "public void callback(%s);" (G.Values.to_string (schema_to_values schema));
 
-  end_class name;
-  name 
+  end_intf name;
+  name
 
 let output_schema_binder index schema =
   match schema with
   | [] -> None
   | _ -> Some (output_schema_binder index schema)
 
-let params_to_values = List.mapi (fun i (n,t) -> param_name_to_string n i, t >> param_type_to_string >> as_cxx_type)
+let params_to_values = List.mapi (fun i (n,t) -> param_name_to_string n i, t >> param_type_to_string)
 let params_to_values = List.unique & params_to_values
-let make_const_values = List.map (fun (name,t) -> name, sprintf "%s const&" t)
 
 let output_value_defs vals =
   vals >> List.iter (fun (name,t) -> output "%s %s;" t name)
-
-let schema_to_values = List.mapi (fun i attr -> name_of attr i, attr.RA.domain >> Type.to_string >> as_cxx_type)
 
 let output_schema_data index schema =
   let name = default_name "data" index in
@@ -84,16 +78,12 @@ let output_schema_data index schema =
   schema >> schema_to_values >> output_value_defs;
   end_class name
 
-let output_value_inits vals =
-  match vals with
-  | [] -> ()
-  | _ ->
-    output " : %s"
-    (String.concat "," (List.map (fun (name,_) -> sprintf "%s(%s)" name name) vals))
-
 let output_params_binder index params =
+(*
   let name = default_name "params" index in
   start_class name;
+*)
+(*
   let values = params_to_values params in
   values >> make_const_values >> output_value_defs;
   empty_line ();
@@ -104,16 +94,15 @@ let output_params_binder index params =
   empty_line ();
   output "void set_params(typename Traits::statement stmt)";
   G.open_curly ();
+*)
   List.iteri set_param params;
+  ()
+(*
   G.close_curly "";
   empty_line ();
   end_class name;
   name
-
-let output_params_binder index params =
-  match params with
-  | [] -> "typename Traits::no_params"
-  | _ -> output_params_binder index params
+*)
 
 type t = unit
 
@@ -121,28 +110,38 @@ let start () = ()
 
 let generate_code () index schema params kind props =
    let schema_binder_name = output_schema_binder index schema in
-   let params_binder_name = output_params_binder index params in
-(*    if (Option.is_some schema_binder_name) then output_schema_data index schema; *)
-(*    out_public (); *)
-(*    if (Option.is_some schema_binder_name) then output "template<class T>"; *)
    let values = params_to_values params in
-   let result = match schema_binder_name with None -> [] | Some _ -> ["result","T&"] in
+   let result = match schema_binder_name with None -> [] | Some name -> ["result",name] in
    let all_params = G.Values.to_string
-     (["db","typename Traits::connection"] @ result @ (make_const_values values)) 
+     (["db","Connection"] @ result @ values)
    in
    let name = choose_name props kind index in
    let sql = G.quote (get_sql props kind params) in
-   let inline_params = G.Values.inline (make_const_values values) in
-   output "static bool %s(%s)" name all_params;
+   output "public static int %s(%s) throws SQLException" name all_params;
    G.open_curly ();
+   output "PreparedStatement pstmt = db.prepareStatement(%s);" sql;
+   output_params_binder index params;
    begin match schema_binder_name with
-   | None -> output "return Traits::do_execute(db,_T(%s),%s(%s));" sql params_binder_name inline_params
-   | Some schema_name ->output "return Traits::do_select(db,result,_T(%s),%s(),%s(%s));" 
-          sql (schema_name ^ "<typename T::value_type>") params_binder_name inline_params
+   | None -> output "return pstmt.executeUpdate();"
+   | Some name ->
+      output "ResultSet res = pstmt.executeQuery();";
+      let args = List.mapi (fun index attr -> get_column attr index) schema in
+      let args = String.concat "," args in
+      output "int count = 0;";
+      output "while (res.next())";
+      G.open_curly ();
+      output "result.callback(%s);" args;
+      output "count++;";
+      G.close_curly "";
+      output "return count;"
    end;
    G.close_curly "";
    empty_line ()
 
-let start_output () = start_class "sqlgg"
+let start_output () =
+  output "import java.sql.*;";
+  empty_line ();
+  start_class "sqlgg"
+
 let finish_output () = end_class "sqlgg"
 
