afbdd8bc853a4e429e8ea83ccae419e8640705f0
ygrek <ygrek@autistici.org>
2009-06-02 21:39:14 +0300

    0.2.1

--- config.ml
+++ config.ml
@@ -4,9 +4,9 @@ open ListMore
 open ExtString
 open Operators
 
-let version_number = "0.2.0+"
+let version_number = "0.2.1"
 
-let version = Printf.sprintf "%s (%s)" 
+let version = Printf.sprintf "%s (%s)"
     version_number
     (Git.revision >> String.explode >> List.take 8 >> String.implode)
 
