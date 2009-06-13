cdac22516a67fef298c466e96edb1213a3872b7f
ygrek <ygrek@autistici.org>
2009-06-13 18:24:17 +0300

    csharp demo

--- demo/demo_csharp.cs
+++ demo/demo_csharp.cs
@@ -1,60 +1,19 @@
 using System.Data;
 using MySql.Data.MySqlClient;
 
-namespace sqlgg
+public class Test
 {
-  public class select_all
-  {
-    IDbCommand cmd;
-    string sql = "SELECT name, surname FROM person LIMIT @lim";
+   public static void Main(string[] args)
+   {
+      string connectionString = "Server=localhost;Database=test;User ID=root;Password=;";
+      IDbConnection conn = new MySqlConnection(connectionString);
+      conn.Open();
 
-    public select_all(IDbConnection aConn)
-    {
-      cmd = aConn.CreateCommand();
-      cmd.CommandText = sql;
-      IDbDataParameter p = cmd.CreateParameter();
-      p.ParameterName = "@lim";
-      p.DbType = DbType.Int32;
-      cmd.Parameters.Add(p);
-      cmd.Prepare();
-    }
+      sqlgg.calc_total calc = new sqlgg.calc_total(conn);
+      calc.execute(delegate (string name, int total) { System.Console.WriteLine(name + ": " + total); });
 
-    public delegate void func(string name, string surname);
-
-    public void execute(int limit, func f)
-    {
-       ((IDbDataParameter)cmd.Parameters[0]).Value = limit;
-       IDataReader reader = cmd.ExecuteReader();
-       while (reader.Read())
-       {
-            string name = (string)reader["name"];
-            string surname = (string)reader["surname"];
-            f(name,surname);
-       }
-       reader.Close();
-    }
-  }
+      conn.Close();
+      conn = null;
+   }
 }
 
- public class Test
- {
-    public static void Main(string[] args)
-    {
-       string connectionString =
-          "Server=localhost;" +
-          "Database=test;" +
-          "User ID=root;" +
-          "Password=;" +
-          "Pooling=false";
-       IDbConnection conn = new MySqlConnection(connectionString);
-       conn.Open();
-
-       sqlgg.select_all sel = new sqlgg.select_all(conn);
-       sel.execute(10, delegate (string name, string surname) {
-         Console.WriteLine("Name: " + name + " " + surname); });
-
-       // clean up
-       conn.Close();
-       conn = null;
-    }
- }
