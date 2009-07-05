99f5387f63a38c51b9c9e522eac48fb805cf2592
ygrek <ygrek@autistici.org>
2009-07-05 14:56:57 +0300

    test explicit VALUES

--- test.ml
+++ test.ml
@@ -7,26 +7,27 @@ open ListMore
 
 let named s = (Some s,(0,0))
 let param = (None,(0,0))
+let p name t = (named name, Some t)
 
 let cmp_params p1 p2 =
   try
-    List.for_all2 (fun ((name1,pos1),t1) ((name2,pos2),t2) -> 
+    List.for_all2 (fun ((name1,pos1),t1) ((name2,pos2),t2) ->
       name1 = name2 && t1 = t2 && pos1 = (0,0) && snd pos2 > fst pos2)
     p1 p2
   with
     _ -> false
 
-let tt ?msg stmt schema params =
+let tt ?msg sql schema params =
   let print_schema = RA.Schema.to_string in
   let print_params = Stmt.params_to_string in
-  let (s1,p1,_) =
-    try
-      Parser.parse_stmt stmt
-    with
-    | _ -> assert_failure "tt failed"
-  in
-  assert_equal ?msg ~printer:print_schema schema s1;
-  assert_equal ?msg ~cmp:cmp_params ~printer:print_params params p1
+  let stmt = Main.parse_one_exn (sql,[]) in
+  assert_equal ?msg ~printer:print_schema schema stmt.schema;
+  assert_equal ?msg ~cmp:cmp_params ~printer:print_params params stmt.params
+
+let wrong sql =
+  ("Expected error in : " ^ sql) @? (try ignore (Main.parse_one_exn (sql,[])); false with exn -> true)
+
+(* let wrong sql = assert_equal None (Main.parse_one (sql,[])) *)
 
 let test () =
   tt "CREATE TABLE test (id INT, str TEXT, name TEXT)"
@@ -44,6 +45,17 @@ let test () =
   tt "select test.name from test where test.id + @x = ? or test.id - @x = ?"
      [attr "name" Text;]
      [named "x", Some Int; param, Some Int; named "x", Some Int; param, Some Int;];
+  tt "insert into test values"
+     []
+     [p "id" Int; p "str" Text; p "name" Text];
+  tt "insert into test (str,name) values"
+     []
+     [p "str" Text; p "name" Text];
+  tt "insert into test values (2,'hello' || ' world',@name)"
+     []
+     [p "name" Text];
+  wrong "insert into test values (1,2)";
+  wrong "insert into test (str,name) values (1,'str','name')";
   ()
 
 (*
@@ -56,17 +68,17 @@ let test_join_result_cols () =
   tt "CREATE TABLE t1 (i INT, j INT)" [] [];
   tt "CREATE TABLE t2 (k INT, j INT)" [] [];
   tt "SELECT * FROM t1 JOIN t2 ON i=t1.j" (ints ["i";"j";"k";"j"]) [];
-  tt ~msg:"NATURAL JOIN" 
+  tt ~msg:"NATURAL JOIN"
     "SELECT * FROM t1 NATURAL JOIN t2" (ints ["j";"i";"k"]) [];
-  tt ~msg:"JOIN USING" 
+  tt ~msg:"JOIN USING"
     "SELECT * FROM t1 JOIN t2 USING (j)" (ints ["j";"i";"k"]) [];
   tt ~msg:"NATURAL JOIN with common column in WHERE"
-    "SELECT * FROM t1 NATURAL JOIN t2 WHERE j > @x" 
-    (ints ["j";"i";"k"]) 
+    "SELECT * FROM t1 NATURAL JOIN t2 WHERE j > @x"
+    (ints ["j";"i";"k"])
     [named "x",Some Int];
   tt ~msg:"NATURAL JOIN with common column qualified in WHERE"
-    "SELECT * FROM t1 NATURAL JOIN t2 WHERE t2.j > @x" 
-    (ints ["j";"i";"k"]) 
+    "SELECT * FROM t1 NATURAL JOIN t2 WHERE t2.j > @x"
+    (ints ["j";"i";"k"])
     [named "x",Some Int];
   ()
 
