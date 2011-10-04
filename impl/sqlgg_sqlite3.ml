d97933d47f5a70a2cdfe912f7b2946483e793763
ygrek <ygrek@autistici.org>
2011-10-04 14:19:31 +0300

    sqlite3 traits: show sql text in exceptions

--- impl/sqlgg_sqlite3.ml
+++ impl/sqlgg_sqlite3.ml
@@ -4,7 +4,7 @@ open Printf
 
 module S = Sqlite3
 
-type statement = S.stmt
+type statement = S.stmt * string
 type connection = S.db
 type params = statement
 type row = statement
@@ -16,24 +16,24 @@ type any = string
 
 exception Oops of string
 
-let get_column_Int stmt index = 
+let get_column_Int (stmt,sql) index =
   match S.column stmt index with
   | S.Data.INT i -> i
-  | _ -> raise (Oops (sprintf "get_column_Int %u" index))
+  | _ -> raise (Oops (sprintf "get_column_Int %u for %s" index sql))
 
-let get_column_Text stmt index =
+let get_column_Text (stmt,_) index =
   let x = S.column stmt index in
   S.Data.to_string x
 
 let get_column_Any = get_column_Text
 
-let test_ok rc = 
+let test_ok sql rc =
   if rc <> S.Rc.OK then
-    raise (Oops (sprintf "test_ok %s" (S.Rc.to_string rc)))
+    raise (Oops (sprintf "test_ok %s for %s" (S.Rc.to_string rc) sql))
 
-let bind_param d stmt index = 
+let bind_param d (stmt,sql) index =
   let rc = S.bind stmt (index+1) d in
-  test_ok rc
+  test_ok sql rc
 
 let start_params stmt _ = stmt
 let finish_params _ = ()
@@ -52,32 +52,32 @@ let finally final f x =
     final ();
     r
 
-let select db sql set_params callback =
+let with_sql db sql f =
   let stmt = S.prepare db sql in
-  finally (fun () -> test_ok (S.finalize stmt))
-  (fun () ->
-    set_params stmt;
-    while S.Rc.ROW = S.step stmt do
-      callback stmt
-    done) ()
+  finally
+    (fun () -> test_ok sql (S.finalize stmt))
+    f (stmt,sql)
 
-let execute db sql set_params = 
-  let stmt = S.prepare db sql in
-  finally (fun () -> test_ok (S.finalize stmt))
-  (fun () ->
+let select db sql set_params callback =
+  with_sql db sql (fun stmt ->
     set_params stmt;
-    let rc = S.step stmt in
+    while S.Rc.ROW = S.step (fst stmt) do
+      callback stmt
+    done)
+
+let execute db sql set_params =
+  with_sql db sql (fun stmt ->
+    set_params stmt;
+    let rc = S.step (fst stmt) in
     if rc <> S.Rc.DONE then raise (Oops (sprintf "execute : %s" sql));
     Int64.of_int (S.changes db)
-  ) ()
+  )
 
 let select1 db sql set_params callback =
-  let stmt = S.prepare db sql in
-  finally (fun () -> test_ok (S.finalize stmt))
-  (fun () ->
+  with_sql db sql (fun stmt ->
     set_params stmt;
-    if S.Rc.ROW = S.step stmt then
+    if S.Rc.ROW = S.step (fst stmt) then
       Some (callback stmt)
     else
-      None) ()
+      None)
 
