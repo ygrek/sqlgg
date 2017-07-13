3d85e63b7344c9035a47fec26664c0cd1f06f3b2
ygrek <ygrek@autistici.org>
2017-07-12 18:53:58 -0700

    do not identify build user in version

--- myocamlbuild_config.ml
+++ myocamlbuild_config.ml
@@ -2,7 +2,7 @@ open Mybuild
 
 let () =
   OCaml.setup();
-  Version.save ~default:"Version_release.id" "src/version.ml";
+  Version.save ~identify:false ~default:"Version_release.id" "src/version.ml";
   ()
 
 (* mark_tag_used is not available in ocaml < 4.02 *)
