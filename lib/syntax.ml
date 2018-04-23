ygrek/sqlgg

commit 33dfd07b96aabed6b928d226d0be3237da3b5e0f

sql: improve single-row detection (fix #47)

lib/syntax.ml
@@ -38,13 +38,18 @@ let get_params_q e =
   in
   loop [] e |> List.rev
 
+let rec is_singular = function
+| Value _
+| Param _ -> true
+| Column _ -> false
+| Fun (func,args) ->
+  (* grouping function of zero or single parameter or function of all singular values *)
+  (Type.is_grouping func && List.length args <= 1) || List.for_all is_singular args
+| Select _ -> false (* ? *)
+| Inserted _ -> false (* ? *)
+
 let test_all_grouping columns =
-  let test = function
-  (* grouping function of zero or single parameter *)
-  | Expr (Fun (func,args),_) when Type.is_grouping func && List.length args <= 1 -> true
-  | _ -> false
-  in
-  List.for_all test columns
+  List.for_all (function Expr (e,_) -> is_singular e | All | AllOf _ -> false) columns
 
 let cross = List.fold_left Schema.cross []
 

test/misc.sql
@@ -46,3 +46,6 @@ INSERT INTO test VALUES
 (1, 'one'),
 (2, 'two'),
 (3, 'three');
+
+-- @issue47
+SELECT count(*) > 0 FROM test;

test/out/function.xml
@@ -5,7 +5,7 @@
   <in/>
   <out/>
  </stmt>
- <stmt name="select_1" sql="SELECT increment(2)" kind="select" cardinality="0,1">
+ <stmt name="select_1" sql="SELECT increment(2)" kind="select" cardinality="1">
   <in/>
   <out>
    <value name="_0" type="Int"/>

test/out/misc.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 
 <sqlgg>
- <stmt name="select_0" sql="SELECT strftime('%s','now')" kind="select" cardinality="0,1">
+ <stmt name="select_0" sql="SELECT strftime('%s','now')" kind="select" cardinality="1">
   <in/>
   <out>
    <value name="_0" type="Text"/>
@@ -108,7 +108,7 @@
   <in/>
   <out/>
  </stmt>
- <stmt name="select_19" sql="SELECT $function$&#x0A;BEGIN&#x0A;    RETURN ($1 ~ $q$[\t\r\n\v\\]$q$);&#x0A;END;&#x0A;$function$" kind="select" cardinality="0,1">
+ <stmt name="select_19" sql="SELECT $function$&#x0A;BEGIN&#x0A;    RETURN ($1 ~ $q$[\t\r\n\v\\]$q$);&#x0A;END;&#x0A;$function$" kind="select" cardinality="1">
   <in/>
   <out>
    <value name="_0" type="Text"/>
@@ -121,5 +121,11 @@
  <stmt name="insert_test_21" sql="INSERT INTO test VALUES&#x0A;(1, 'one'),&#x0A;(2, 'two'),&#x0A;(3, 'three')" kind="insert" target="test" cardinality="0">
   <in/>
   <out/>
+ </stmt>
+ <stmt name="issue47" sql="SELECT count(*) &gt; 0 FROM test" kind="select" cardinality="1">
+  <in/>
+  <out>
+   <value name="_0" type="Bool"/>
+  </out>
  </stmt>
 </sqlgg>

commit 95c753c26df9bd48c4388b26196533376e86e95a

fix single-row detection
broken in 33dfd07

lib/syntax.ml
@@ -39,18 +39,18 @@ let get_params_q e =
   in
   loop [] e |> List.rev
 
-let rec is_singular = function
+let rec is_grouping = function
 | Value _
-| Param _ -> true
-| Column _ -> false
+| Param _
+| Column _
+| Select _
+| Inserted _ -> false
 | Fun (func,args) ->
-  (* grouping function of zero or single parameter or function of all singular values *)
-  (Type.is_grouping func && List.length args <= 1) || List.for_all is_singular args
-| Select _ -> false (* ? *)
-| Inserted _ -> false (* ? *)
+  (* grouping function of zero or single parameter or function on grouping result *)
+  (Type.is_grouping func && List.length args <= 1) || List.exists is_grouping args
 
-let test_all_grouping columns =
-  List.for_all (function Expr (e,_) -> is_singular e | All | AllOf _ -> false) columns
+let exists_grouping columns =
+  List.exists (function Expr (e,_) -> is_grouping e | All | AllOf _ -> false) columns
 
 let cross = List.fold_left Schema.cross []
 
@@ -282,14 +282,15 @@ and eval_select env { columns; from; where; group; having; } =
     | Some (t,l) -> join env (resolve_source env t, List.map (fun (x,k) -> resolve_source env x, k) l)
     | None -> env, []
   in
-  let singlerow = group = [] && test_all_grouping columns in
-  let singlerow2 = where = None && group = [] && test_all_const columns in
+  let cardinality =
+    if from = None then (if where = None then `One else `Zero_one)
+    else if group = [] && exists_grouping columns then `One
+    else `Nat
+  in
   let p1 = params_of_columns env columns in
   let p3 = get_params_opt env where in
   let p4 = get_params_l env group in
   let p5 = get_params_opt env having in
-  let cardinality = if singlerow then `One else
-                    if singlerow2 then `Zero_one else `Nat in
   (infer_schema env columns, p1 @ p2 @ p3 @ p4 @ p5, env.tables, cardinality)
 
 and resolve_source env (x,alias) =

src/test.ml
@@ -100,9 +100,13 @@ let test4  () =
   tt "select max(x,y) from test4 limit 1" a [] ~kind:(Select `Zero_one);
   tt "select max(x,y) from test4 limit 2" a [] ~kind:(Select `Nat);
 *)
-  tt "select 1+2 from test4" a [] ~kind:(Select `Zero_one);
+  tt "select 1" a [] ~kind:(Select `One);
+  tt "select greatest(1+2,10)" a [] ~kind:(Select `One);
+  tt "select greatest(1+2,10) where 1 = 2" a [] ~kind:(Select `Zero_one);
+  tt "select 1 from test4" a [] ~kind:(Select `Nat);
+  tt "select 1+2 from test4" a [] ~kind:(Select `Nat);
   tt "select least(10+unix_timestamp(),random()), concat('test',upper('qqqq')) from test"
-    [attr "" Int; attr "" Text] [] ~kind:(Select `Zero_one);
+    [attr "" Int; attr "" Text] [] ~kind:(Select `Nat);
   tt "select greatest(10,x) from test4" a [] ~kind:(Select `Nat);
   tt "select 1+2 from test4 where x=y" a [] ~kind:(Select `Nat);
   tt "select max(x) as q from test4 where y = x + @n" [attr "q" Int] [named "n", Int] ~kind:(Select `One);