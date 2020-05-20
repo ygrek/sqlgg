831725731301913f3c5ad38def34c31854f3fac9
ygrek <ygrek@autistici.org>
2020-05-20 17:21:57 -0400

    cli: fix debug

--- src/cli.ml
+++ src/cli.ml
@@ -95,7 +95,7 @@ let main () =
     "-gen", Arg.String set_out, "cxx|caml|java|xml|csharp|none Set output language (default: none)";
     "-name", Arg.String (fun x -> name := x), "<identifier> Set output module name (default: sqlgg)";
     "-params", Arg.String set_params_mode, "named|unnamed|oracle|postgresql|none Output query parameters substitution (default: none)";
-    "-debug", Arg.Int (fun x -> Sqlgg_config.debug_level := x), "<N> set debug level";
+    "-debug", Arg.Int Sqlgg_config.set_debug_level, "<N> set debug level";
     "-no-header", Arg.Unit (fun () -> Sqlgg_config.gen_header := None),
       "do not put version header in generated output";
     "-no-header-timestamp", Arg.Unit (fun () -> Sqlgg_config.gen_header := Some `Without_timestamp),
