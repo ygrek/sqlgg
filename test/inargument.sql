2b3fdea994b41e1eb0bb0554ee4c76043f5d5753
Mauricio Fernández <mfp@acm.org>
2021-11-09 23:09:10 +0100

    add tests for IN @foo codegen

---
+++ test/inargument.sql
@@ -0,0 +1,33 @@
+-- @create_table_foo
+CREATE TABLE foo(
+    id INTEGER PRIMARY KEY,
+    foo TEXT NULL
+);
+
+-- @create_table_bar
+CREATE TABLE bar(
+    foo_id INTEGER NOT NULL,
+    baz TEXT NOT NULL
+);
+
+-- @find
+SELECT * FROM foo
+WHERE id IN @ids;
+
+-- @get
+SELECT * FROM foo
+WHERE id IN @ids
+LIMIT 1;
+
+-- @find2
+SELECT * FROM foo
+WHERE id IN @ids AND foo NOT IN @foos;
+
+-- @get2
+SELECT * FROM foo
+WHERE id IN @ids AND foo NOT IN @foos;
+
+-- @join
+SELECT *
+FROM foo f JOIN bar b ON f.id = b.foo_id
+WHERE b.baz IN @bazz AND b.baz NOT IN @notbazz AND LENGTH(f.foo) IN @lengths;
